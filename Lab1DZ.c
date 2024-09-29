377416a728486027b59d63721bf03594b6f4066c
VitalyID242 <agatxx12@gmail.com>
2024-09-30 00:38:07 +0300

    Add files via upload

---
+++ Lab1DZ.c
@@ -0,0 +1,14 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts("                /L        ");
+	puts("               /  L       ");
+	puts("              /    L      ");
+	puts("             /______L     ");
+	puts("             |      |     ");
+	puts("             | ___  |     ");
+	puts("             | |.|  |     ");
+	puts("             |_| |__|     ");
+}
